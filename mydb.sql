7f35a4e52f6689a849ab391a7f84c7aada87f9fe
vbl10 <99830114+vbl10@users.noreply.github.com>
2024-04-09 16:28:38 -0300

    est_usr.nome => est.nome; est_usr.direitos

--- mydb.sql
+++ mydb.sql
@@ -9,15 +9,22 @@ CREATE TABLE usuario (
 
 CREATE TABLE estacao (
     id char(8) PRIMARY KEY,
-    localizacao varchar(30)
+    localizacao varchar(30),
+    nome varchar(30)
 );
 
 CREATE TABLE estacao_usuario (
     id_usuario INT REFERENCES usuario(id),
     id_estacao char(8) REFERENCES estacao(id),
-    PRIMARY KEY (id_usuario, id_estacao),
+    PRIMARY KEY (id_usuario, id_estacao)
 
-    nome varchar(30)
+    /*
+    00000000b => direito de leitura
+    00000001b => direito de alterar nome e localização da estação
+    00000010b => direito de alterar direitos
+    00010000b => direito de enviar comandos (cli)
+    */
+    direitos BINARY(1) DEFAULT 0;
 );
 
 CREATE TABLE cli_entrada (
@@ -43,12 +50,12 @@ CREATE TABLE leitura (
     id_estacao char(8) REFERENCES estacao(id),
     PRIMARY KEY (id, id_estacao),
     
-    umidade_ar float,
-    temperatura float,
-    umidade_solo float,
-    pluv_indice float,
+    umidade_ar float NOT NULL,
+    temperatura float NOT NULL,
+    umidade_solo float NOT NULL,
+    pluv_indice float NOT NULL,
     
-    horario DATETIME
+    horario DATETIME NOT NULL
 );
 
 CREATE TABLE controlador (
